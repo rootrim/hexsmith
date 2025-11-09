cd235494e3476bb1eb182717ef85b323d65bbf19
nixtrim <omuaz01@gmail.com>
2025-11-09 22:02:40 +0300

    added first lines of code

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description =
+    "Hexsmith - interactive tui for editing and using payloads, shellcodes and any other hex codes on binaries";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+  };
+
+  outputs = { self, nixpkgs, naersk }:
+    let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      naerskLib = pkgs.callPackage naersk { };
+    in {
+
+      packages.x86_64-linux.default = naerskLib.buildPackage {
+        src = ./.;
+        buildInputs = with pkgs; [ openssl ];
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+      };
+    };
+}
