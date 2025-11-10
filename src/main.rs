380bfffba5aec931fe5b19bfb5f11e8bf66ce1d3
nixtrim <omuaz01@gmail.com>
2025-11-10 20:32:51 +0300

    removed everything

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-mod app;
-mod ui;
-
-use anyhow::Context;
-use app::App;
-
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    let mut terminal = ratatui::init();
-
-    let binary_path = std::env::args()
-        .nth(1)
-        .context("Please provide a binary to analyze")?;
-
-    let mut app = App::new(binary_path);
-
-    app.run(&mut terminal).await?;
-
-    ratatui::restore();
-
-    Ok(())
+fn main() {
+    println!("habbab");
 }
